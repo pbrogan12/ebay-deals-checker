10a3eab05a4828968069b3119ff9a08aa8625ce9
pbrogan <pbrogan12@gmail.com>
2013-07-02 16:44:07 -0400

    Now can send email.

--- ebay.py
+++ ebay.py
@@ -1,22 +1,37 @@
-import urllib, json
+import urllib, json, smtplib
 import cPickle as pickle
-
+from email.mime.text import MIMEText
 def dealsChecker(searchTerm, emailAddress):
+
     try:
         itemIds = pickle.load(open('itemIds.pkl','rb'))
     except:
         itemIds = []
+
+    msg = ""
     itemData = urllib.urlopen('http://deals.ebay.com/feeds/json').read()
     itemData = itemData.replace('ebaydailydeals', '"ebaydailydeals"')
     itemData = itemData.replace('(', '')
     itemData = itemData.replace(')', '')
     itemData = itemData.replace(';', '')
     itemData = json.loads(itemData)
+
     for i in itemData['ebaydailydeals']['items']:
         if searchTerm.lower() in i['title'].lower():
             if str(i['itemid']) in itemIds:
                 pass
             else:
                 itemIds.append(str(i['itemid']))
-                print i['title'], i['convertedcurrentprice'], i['itemid']
+                itemInfo = i['title'], i['convertedcurrentprice'], i['itemid'], i['dealurl']
+                msg = msg + str(itemInfo)
     pickle.dump(itemIds, open('itemIds.pkl', 'wb'))
+
+    if msg:
+        msg = MIMEText(msg)
+        msg['Subject'] = 'Ebay daily deals for %s' % searchTerm
+        msg['From'] = 'ebayDealsChecker@localhost'
+        msg['To'] = emailAddress
+        s = smtplib.SMTP('localhost')
+        s.sendmail('ebayDealsCHecker@localhost',emailAddress, msg.as_string())
+        s.quit
+
